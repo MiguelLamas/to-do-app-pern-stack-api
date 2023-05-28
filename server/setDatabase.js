47a79ff2ccd28f98f84b238437e915f59cefd6ae
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-28 18:01:50 +0100

    using .ENV for connectionString for security reasons

--- server/setDatabase.js
+++ server/setDatabase.js
@@ -1,7 +1,8 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: "postgres://miguellamas:xZ0iwBKiOE4UEclvFskfphqrPd5lt7B9@dpg-chn8qo9mbg5577k4da40-a.frankfurt-postgres.render.com/perntodo_epek?ssl=true"
+  connectionString: process.env.DATABASE_URL,
 }  
 );
 
